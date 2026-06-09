03138b7f8e76ea1200db798e53c21155de73ab15
ibmmqmet <marke_taylor@uk.ibm.com>
2026-06-09 08:23:14 +0100

    Update mqmetric copyright dates

--- mqmetric/globals.go
+++ mqmetric/globals.go
@@ -6,7 +6,7 @@ storage mechanisms including Prometheus and OpenTelemetry.
 package mqmetric
 
 /*
-  Copyright (c) IBM Corporation 2016, 2024
+  Copyright (c) IBM Corporation 2016, 2026
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
--- mqmetric/queue.go
+++ mqmetric/queue.go
@@ -1,7 +1,7 @@
 package mqmetric
 
 /*
-  Copyright (c) IBM Corporation 2018,2025
+  Copyright (c) IBM Corporation 2018,2026
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
